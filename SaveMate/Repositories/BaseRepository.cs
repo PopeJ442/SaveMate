36c1abb7398c5c63851756968eb5251bbbd1ec71
popej442 <hpi78generation@gmail.com>
2025-03-03 15:30:33 +0000

    Initial commit

---
+++ SaveMate/Repositories/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaveMate.ApplicationDbContext;
+using System;
+using System.Runtime.InteropServices;
+
+namespace SaveMate.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly SaveMateDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(SaveMateDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+         public async Task<T> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await SaveAsync();
+        }
+
+        public async Task DeleteAsync(object id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await SaveAsync();
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
