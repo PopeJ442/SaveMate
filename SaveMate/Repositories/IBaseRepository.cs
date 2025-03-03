0b8473a1c53f7e9e2e8e92b467c8e93014269ed5
popej442 <hpi78generation@gmail.com>
2025-03-03 15:30:33 +0000

    Initial commit

---
+++ SaveMate/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SaveMate.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(object id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(object id);
+        Task SaveAsync();
+    }
+}
