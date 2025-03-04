6954f1bbfcc35392e63c903e049697e40823f260
popej442 <hpi78generation@gmail.com>
2025-03-04 16:34:18 +0000

    Initial commit

---
+++ SaveMate/Repositories/AccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaveMate.ApplicationDbContext;
+using SaveMate.Models;
+using System;
+
+namespace SaveMate.Repositories
+{
+    public class AccountRepository : BaseRepository<Account>, IAccountRepository
+    {
+        public AccountRepository(SaveMateDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Account>> GetAccountsByUserIdAsync(int userId)
+        {
+            return await _context.Accounts.Where(a => a.UserId == userId).ToListAsync();
+        }
+    }
+}
