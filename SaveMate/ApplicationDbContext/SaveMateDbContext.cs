5d61c88dfe0dfd8ffd009bd1209fc93c33e16179
popej442 <hpi78generation@gmail.com>
2025-03-04 16:34:14 +0000

    Fixed: Corrected casing of DbContext properties.

--- SaveMate/ApplicationDbContext/SaveMateDbContext.cs
+++ SaveMate/ApplicationDbContext/SaveMateDbContext.cs
@@ -5,13 +5,13 @@ namespace SaveMate.ApplicationDbContext
 {
     public class SaveMateDbContext(DbContextOptions<SaveMateDbContext> options) : DbContext(options)
     {
-        public DbSet<User> users { get; set; }
-        public DbSet<Account> accounts { get; set; }
-        public DbSet<AccountCustomType> accountCustomTypes { get; set; }
-        public DbSet<Category> categories { get; set; }
-        public DbSet<Goal> goals { get; set; }
-        public DbSet<Budget> budgets { get; set; }
-        public DbSet<Transaction> transactions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountCustomType> AccountCustomTypes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
         
     }
 }
