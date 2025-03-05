c262c806d26fd8db4a3476042cbcb1b7fd728211
popej442 <hpi78generation@gmail.com>
2025-03-05 13:50:50 +0000

    Renamed primary keys in various models.

--- SaveMate/Models/Goal.cs
+++ SaveMate/Models/Goal.cs
@@ -2,7 +2,7 @@
 {
     public class Goal
     {
-        public int Id { get; set; }
+        public int GoalId { get; set; }
 
         public int UserId { get; set; }
         public User User { get; set; }
