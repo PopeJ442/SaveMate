c6d24c1a76ec52065c011a54464599079b01195c
popej442 <hpi78generation@gmail.com>
2025-03-25 16:50:19 +0000

    Initial commit

---
+++ SaveMate/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using SaveMate.ApplicationDbContext;
+using SaveMate.Models;
+using SaveMate.Repositories.IRepository;
+
+namespace SaveMate.Repositories
+{
+    public class UserRepository(SaveMateDbContext context) : BaseRepository<User>(context), IUserRepository
+    {
+    }
+}
