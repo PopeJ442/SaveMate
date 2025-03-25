a0ce859448f73dad0be438893065238d72e42b27
popej442 <hpi78generation@gmail.com>
2025-03-25 16:50:21 +0000

    Initial commit

---
+++ SaveMate/Services/IService/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using SaveMate.Models;
+
+namespace SaveMate.Services.IService
+{
+    public interface IAccountService
+    {
+        Task<Account> GetByIdAsync(Guid id);
+        Task<IEnumerable<Account>> GetAccountsByUserIdAsync(int userId);
+        Task AddAsync(Account account);
+        Task UpdateAsync(Account account);
+        Task DeleteAsync(Guid id);
+    }
+}
