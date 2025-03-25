e96d95bd75c685b634f9d346b8b4398cf838fe23
popej442 <hpi78generation@gmail.com>
2025-03-25 16:50:19 +0000

    Initial commit

---
+++ SaveMate/Repositories/IRepository/IAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using SaveMate.Models;
+
+namespace SaveMate.Repositories.IRepository
+{
+    public interface IAccountRepository : IBaseRepository<Account>
+    {
+        Task<IEnumerable<Account>> GetAccountsByUserIdAsync(int userId);
+    }
+}
