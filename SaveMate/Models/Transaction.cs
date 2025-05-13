fc30635435cf1469d56d723d631b79290655e2c4
popej442 <hpi78generation@gmail.com>
2025-05-13 16:43:14 +0000

    Updated Transaction model and database schema.

--- SaveMate/Models/Transaction.cs
+++ SaveMate/Models/Transaction.cs
@@ -1,15 +1,23 @@
-﻿using SaveMate.Models.Enums;
+﻿using Microsoft.EntityFrameworkCore;
+using SaveMate.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SaveMate.Models
 {
     public class Transaction
     {
+        [Key]
         public int TransactionId { get; set; }
-        public int AccountId { get; set; }
-        public int CategoryId { get; set; }
+        
+        public Guid AccountId { get; set; }
+        public CategoryType Category { get; set; }
         public decimal Amount { get; set; }
         public TransactionType Type { get; set; } 
         public string Description { get; set; } = string.Empty;
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        [ForeignKey("AccountId")]
+        public Account Account { get; set; }
+       
     }
 }
